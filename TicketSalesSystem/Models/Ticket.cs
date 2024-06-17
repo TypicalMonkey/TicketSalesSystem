8b1f6b9aad7d87a0c9364beb2ac201c20cfbf811
Mateusz Ciochoń <mateusz.ciochon.23@gmail.com>
2024-06-17 17:15:15 +0200

    ticket model

--- TicketSalesSystem/Models/Ticket.cs
+++ TicketSalesSystem/Models/Ticket.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicketSalesSystem.Models
 {
     public class Ticket
     {
         public int Id { get; set; }
-        public int TrainId { get; set; }
-        public int RouteId { get; set; }
         public DateTime PurchaseDate { get; set; }
         public decimal Price { get; set; }
+
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
+
+        public int StartStationId { get; set; }
+        public int EndStationId { get; set; }
+        public Station StartStation { get; set; }
+        public Station EndStation { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
